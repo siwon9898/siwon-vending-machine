3969e16486696c6a3dcb56dedc72ec6f2e6bbeaf
leecool <siwon9898@gmail.com>
2025-05-31 15:58:45 +0900

    diagram 세팅

--- src/views/pages/diagram/Diagram.tsx
+++ src/views/pages/diagram/Diagram.tsx
@@ -1,7 +1,17 @@
-import React from "react";
-
+import DiagramImg from "@images/Diagram.png";
+import { Box, styled } from "@mui/material";
 const Diagram = () => {
-  return <div>Diagram</div>;
+  return <DiagramBox />;
 };
+
+const DiagramBox = styled(Box)({
+  width: "100%",
+  height: "100%",
+  background: `url(${DiagramImg}) 50% 50% no-repeat`,
+  backgroundSize: "cover",
+  "& > img": {
+    width: "100%",
+  },
+});
 
 export default Diagram;
